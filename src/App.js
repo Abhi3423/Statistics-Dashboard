a65460372ffe89414eefe9f40c6aaa81d92988dd
Abhishek Yadav <abhishekaug2002@gmail.com>
2022-12-28 18:40:14 +0530

    File changes done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './components/Home/home';
-import Home2 from './components/Home/home2';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/check" element={<Home2/>} />
       </Routes>
     </Router>
   );
